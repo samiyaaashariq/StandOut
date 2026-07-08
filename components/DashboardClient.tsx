2b41e98c5d6f616e2f070dd452d456d2ff7cb011
Samiya Shariq <samiya.shariq31@gmail.com>
2026-07-09 01:54:40 +0530

    Update DashboardClient.tsx

--- components/DashboardClient.tsx
+++ components/DashboardClient.tsx
@@ -1,16 +1,15 @@
 "use client";
-
 import { useState } from "react";
 import ResumeOptimizer from "./ResumeOptimizer";
 import JobMatcher from "./JobMatcher";
-
+import ResumeUpgrader from "./ResumeUpgrader";
 export default function DashboardClient() {
   const [resumeText, setResumeText] = useState("");
-
   return (
     <>
       <ResumeOptimizer onResumeTextChange={setResumeText} />
       <JobMatcher resumeText={resumeText} />
+      <ResumeUpgrader />
     </>
   );
 }
